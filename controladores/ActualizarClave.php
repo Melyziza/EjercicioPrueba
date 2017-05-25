384dce8a628ce7a0c0dd571cfb9a810c5d296ef8
cetecom <cetecom@LC08L1212>
2017-05-25 13:22:33 -0400

    Actualizado

---
+++ controladores/ActualizarClave.php
@@ -0,0 +1,25 @@
+<?php
+   include '../constantes.php';
+   include '../librerias.php';
+?>
+<?php
+        $nomusuario=$_POST['nomusuario'];
+        $passAntigua = $_POST['passAntigua'];
+        $passNueva = $_POST['passNueva'];
+        $passConfirma = $_POST['passConfirma'];
+        
+        if ($passNueva!=$passConfirma){
+            echo "Las claves no son iguales";
+            return; 
+        }
+        $oUsr=new Usuario();
+        $oUsr->nombre=$nomusuario;
+        $oUsr->clave=$passAntigua;
+                
+        if(!$oUsr->VerificaUsuarioClave()){
+           echo "El usuario o clave antigua no corresponde";
+           return; 
+        }
+        
+        echo "Estamos listos para el cambio"
+?>
